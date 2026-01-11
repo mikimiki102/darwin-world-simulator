0520f04471f0b0f320a7d577aa6aa933c48294e5
Mikolaj Walek <mwalek102@gmail.com>
2026-01-11 16:43:24 +0100

    Repairing folder structure

--- DarwinWorld/DarwinWorld/src/main/java/agh/ics/oop/model/util/Pair.java
+++
@@ -1,4 +0,0 @@
-package agh.ics.oop.model.util;
-
-public record Pair<T1, T2>(T1 first, T2 second) {
-}
--- DarwinWorld/DarwinWorld/src/main/java/agh/ics/oop/simulation/SimulationChangeListener.java
+++
@@ -1,5 +0,0 @@
-package agh.ics.oop.simulation;
-
-public interface SimulationChangeListener {
-    void onSimulationChanged(Simulation simulation);
-}
